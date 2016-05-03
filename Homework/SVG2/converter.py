4c15497becd6fc29bdac92b48ba7f6be67033d9f
StijnRobben <robbenstijn@hotmail.com>
2016-05-03 18:30:27 +0200

    Homework

--- Homework/SVG2/converter.py
+++ Homework/SVG2/converter.py
@@ -13,7 +13,7 @@ from collections import OrderedDict
 import json as simplejson
  
 # Open the file and select the worksheet
-wb = xlrd.open_workbook('population.xlsx')
+wb = xlrd.open_workbook('GDPdata.xlsx')
 sh = wb.sheet_by_index(0)
  
 # List to hold the dictionaries
@@ -23,13 +23,23 @@ population_list = []
 for rownum in range(0, sh.nrows):
     population = OrderedDict()
     row_values = sh.row_values(rownum) 
-    population['continent'] = row_values[0]
-    population['people'] = "" + str(row_values[1]) + ""
+    population['name'] = row_values[0]
+    population['fillKey'] = row_values[1]
+    population['country code'] = row_values[2]
+    population['GDP'] = row_values[3]
  
     population_list.append(population)
  
 # Serialize the list of dicts to JSON
-j = simplejson.dumps(population_list)
+json = simplejson.dumps(population_list)
 
-print j
+
+print json
+
+
+
+
+
+
+
 
